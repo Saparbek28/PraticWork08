34e10545440e6bdacb7c978d69b45b462be0069d
Saparbek28 <46023456+Saparbek28@users.noreply.github.com>
2019-03-28 00:44:52 +0600

    adding file

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isInt = false;
+            int start = 0, end = 1;
+            while (!isInt)
+            {
+                Console.Write("Начало: ");
+                isInt = int.TryParse(Console.ReadLine(), out start);
+            }
+            isInt = false;
+            while (!isInt)
+            {
+                Console.Write("Конец: ");
+                isInt = int.TryParse(Console.ReadLine(), out end);
+            }
+
+            RangeOfArray array = new RangeOfArray(start, end);
+
+            for (int i = start; i <= end; i++)
+            {
+                array[i] = 5;
+                Console.WriteLine("array[{0}] - {1}", i, array[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Users.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Users
+    {
+        private int[] entryAttempts;
+
+        public Guid Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public int this[int i]
+        {
+            get
+            {
+                return entryAttempts[i];
+            }
+            set
+            {
+                entryAttempts[i] = value;
+            }
+        }
+    }
+}
